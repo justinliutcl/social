6ecc3bb0d3a0b268051710e0456d104c514fcc36
Your Name <you@example.com>
2018-03-28 22:36:50 +0800

    add
    Signed-off-by: Your Name <you@example.com>

---
+++ app/src/main/java/com/justin/social/RetrofitUtils/DataBean/oneFrag/ShortNewsConfig.java
@@ -0,0 +1,10 @@
+package com.justin.social.RetrofitUtils.DataBean.oneFrag;
+
+import com.justin.social.RetrofitUtils.DataBean.BaseConfig;
+
+/**
+ * Created by ASUS on 2018/3/28.
+ */
+
+public class ShortNewsConfig extends BaseConfig {
+}
